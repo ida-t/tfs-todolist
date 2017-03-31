a291651a8161665ad3fd1400e43a41c5ccf6f285
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2017-03-31 20:51:58 +0300

    add counter, create todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  counterValue = 10;
+
+
+  changeCounterValue() {
+    this.counterValue = 15;
+  }
+
+  changeCounter(newValue) {
+    this.counterValue = newValue;
+  }
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  @Input() counterValue;
+  @Output() counterChange = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  increase() {
+    this.counterValue++;
+    this.counterChange.emit(this.counterValue);
+  }
+
+  decrease() {
+    this.counterValue = Math.max(this.counterValue - 1, 0);
+    this.counterChange.emit(this.counterValue);
+  }
+
+}
